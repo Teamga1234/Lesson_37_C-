d6cc8c4e2dde750b0b2b6610c354c28acfe5b83b
VladOlhovsk <olhovskijvladislav27@gmail.com>
2024-12-04 17:46:49 +0200

    main with AudioCd

--- Lesson_37_C++/Lesson_37_C++.cpp
+++ Lesson_37_C++/Lesson_37_C++.cpp
@@ -1,10 +1,10 @@
 ﻿#include "MyQueue.h"
-
-
+#include "AudioCD.h"
+#include <vector>
 
 int main()
 {
-	myQueue::Queue<int> a;
+	/*myQueue::Queue<int> a;
 	a.show();
 
 	a.push_back(9);
@@ -16,5 +16,19 @@ int main()
 		cout << "First: " << a.first() << endl;
 		a.show();
 		a.pop_front();
+	}*/
+
+	std::vector<AudioCD> allMusic;
+	std::ifstream file("music.txt");
+	if (file.is_open())
+	{
+		while (!file.eof())
+		{
+			AudioCD item;
+			item.load(file);
+			allMusic.push_back(item);
+		}
+		file.close();
 	}
+	cout << "Size: " << allMusic.size() << endl;
 }
